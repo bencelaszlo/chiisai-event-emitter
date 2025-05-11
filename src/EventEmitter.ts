2c0e82f2eb33d41d0b11fbb170bb479a6ab30911
bencelaszlo <bencelaszlo@protonmail.com>
2025-05-11 10:11:10 +0200

    build: replace eslint with biome
    replace eslint and its plugins with biome

--- src/EventEmitter.ts
+++ src/EventEmitter.ts
@@ -1,64 +1,65 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// biome-ignore lint/suspicious/noExplicitAny: temp until safe event map is implemented
 export type EventCallback<T = void> = (...args: Array<any>) => T;
 
 export class Subscriptions extends Map<
-  string,
-  Array<{ id: symbol; callback: EventCallback }>
+	string,
+	Array<{ id: symbol; callback: EventCallback }>
 > {
-  get(key: string): { id: symbol; callback: EventCallback }[] {
-    return super.get(key) || [];
-  }
+	get(key: string): { id: symbol; callback: EventCallback }[] {
+		return super.get(key) || [];
+	}
 }
 
 export type Unsubscribe = () => void;
 
 export class EventEmitter {
-  subscriptions: Subscriptions;
+	subscriptions: Subscriptions;
 
-  constructor() {
-    this.subscriptions = new Subscriptions();
-  }
+	constructor() {
+		this.subscriptions = new Subscriptions();
+	}
 
-  /**
-   * @param {string} eventName
-   * @param {EventCallback} callback
-   * @return {Unsubscribe}
-   */
-  subscribe(eventName: string, callback: EventCallback): Unsubscribe {
-    const id = Symbol(callback.toString());
-    this.subscriptions.set(eventName, [
-      ...this.subscriptions.get(eventName),
-      { id, callback },
-    ]);
+	/**
+	 * @param {string} eventName
+	 * @param {EventCallback} callback
+	 * @return {Unsubscribe}
+	 */
+	subscribe(eventName: string, callback: EventCallback): Unsubscribe {
+		const id = Symbol(callback.toString());
+		this.subscriptions.set(eventName, [
+			...this.subscriptions.get(eventName),
+			{ id, callback },
+		]);
 
-    return () =>
-      this.subscriptions.set(
-        eventName,
-        this.subscriptions
-          .get(eventName)
-          .filter(({ id: subscriptionId }) => subscriptionId !== id)
-      );
-  }
+		return () =>
+			this.subscriptions.set(
+				eventName,
+				this.subscriptions
+					.get(eventName)
+					.filter(({ id: subscriptionId }) => subscriptionId !== id),
+			);
+	}
 
-  /**
-   * @param {string} eventName
-   * @param {Array<any>} args
-   * @returns void
-   */
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  emit(eventName: string, ...args: Array<any>): void {
-    this.subscriptions
-      .get(eventName)
-      .forEach(({ callback }) => callback(...args));
-  }
-  /**
-   *
-   * @param eventName eventName to be deleted
-   * @returns void
-   */
-  clear(eventName: string): void {
-    this.subscriptions = new Subscriptions(
-      [...this.subscriptions].filter(([key]) => key !== eventName)
-    );
-  }
+	/**
+	 * @param {string} eventName
+	 * @param {Array<any>} args
+	 * @returns void
+	 */
+	// biome-ignore lint/suspicious/noExplicitAny: temp until safe event map is implemented
+	emit(eventName: string, ...args: Array<any>): void {
+		// biome-ignore lint/complexity/noForEach:
+		this.subscriptions
+			.get(eventName)
+			.forEach(({ callback }) => callback(...args));
+	}
+	/**
+	 *
+	 * @param eventName eventName to be deleted
+	 * @returns void
+	 */
+	clear(eventName: string): void {
+		this.subscriptions = new Subscriptions(
+			[...this.subscriptions].filter(([key]) => key !== eventName),
+		);
+	}
 }
